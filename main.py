ace27468ef32dee90509d10ea7f4f6fdf48b0122
lostliam <95007412+lostliam@users.noreply.github.com>
2023-09-10 16:00:20 +0800

    F

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import websocket
 import time
+import re,random
+websocket.enableTrace(False)
+classids = [
+    '42/lookTime,["message",{"data":{"cid":18538,"csid":73008,"upid":"6255520","type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":94.982709,"key":"286847521544157","reqSource":0,"wid":null,"moldType":0}}]',
+    '42/lookTime,["message",{"data":{"cid":15214,"csid":53700,"upid":"6255520","type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":3.905758,"key":"352559868122590","reqSource":0,"wid":null,"moldType":0}}]',
+    '42/lookTime,["message",{"data":{"cid":18482,"csid":72776,"upid":6255520,"type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":14.952045,"key":"420577864522462","reqSource":0,"wid":null,"moldType":0}}]',
+    '42/lookTime,["message",{"data":{"cid":6449,"csid":18459,"upid":6255520,"type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":3.904323,"key":"344012491077215","reqSource":0,"wid":null,"moldType":0}}]',
+    '42/lookTime,["message",{"data":{"cid":17938,"csid":69285,"upid":6255520,"type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":3.92059,"key":"355744184163808","reqSource":0,"wid":null,"moldType":0}}]',
+    '42/lookTime,["message",{"data":{"cid":17949,"csid":69357,"upid":6255520,"type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":3.854015,"key":"296716742604587","reqSource":0,"wid":null,"moldType":0}}]',
+    '42/lookTime,["message",{"data":{"cid":5878,"csid":16523,"upid":6255520,"type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":3.893309,"key":"952537441592507","reqSource":0,"wid":null,"moldType":0}}]',
+    '42/lookTime,["message",{"data":{"cid":5879,"csid":16526,"upid":6255520,"type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":3.920325,"key":"191903525324193","reqSource":0,"wid":null,"moldType":0}}]'
+]
 
+# 将相同的消息内容用变量代替
 message1 = "40/lookTime"
 message4 = '42/lookTime,["message",{"data":{"cid":18482,"csid":72776,"upid":"6255520","type":1,"token":"9a3965dc181099313f1ca47eb94c318c","time":11,"key":"633277192270593","reqSource":0,"wid":null,"moldType":0}}]'
 message3 = "2"
 message4 = '42/lookTime,["message",{"data":{"cid":15214,"csid":53700,"upid":"6255520","type":4,"token":"9a3965dc181099313f1ca47eb94c318c","time":3.905758,"key":"352559868122590","reqSource":0,"wid":null,"moldType":0}}]'
 def on_message(ws, message):
-    print(message)
+    global message4
     if message.startswith("0"):
         time.sleep(3)
         ws.send(message1)
@@ -14,6 +27,12 @@ def on_message(ws, message):
         time.sleep(3)
         ws.send(message4)
     elif message.startswith("42/lookTime"):
+        print(message)
+        match = re.search(r'"lookTime":(\d+)', message)
+        if match:
+            look_time = match.group(1)
+            if int(look_time)> 5*60*60:
+                message4=random.choice(classids)
         time.sleep(3)
         ws.send(message3)
     elif message.startswith("3"):
@@ -31,7 +50,7 @@ def on_open(ws):
 
 if __name__ == "__main__":
     wssurl = "wss://newwebnew.zgzjzj.com/socket.io/?token=9a3965dc181099313f1ca47eb94c318c&cid=18482&csid=72778&type=1&upid=6255520&key=615995779790336&wid=NaN&reqSource=0&EIO=3&transport=websocket"
-    websocket.enableTrace(True)
+    websocket.enableTrace(False)
     ws = websocket.WebSocketApp(wssurl,
                                 on_message=on_message,
                                 on_error=on_error,
